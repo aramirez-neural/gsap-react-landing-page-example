b1cf717b4c5c0c31e4251525ad54aacf37484915
aramirez-neural <aramirez@neural.es>
2025-05-30 10:15:09 +0200

    Add Footer component with social links and copyright information

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import GalleryCarousel from "./components/GalleryCarousel";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 export default function App() {
   return (
-    <div className="text-2xl text-center text-blue-500">
+    <div>
       <Header />
 
       <main>
@@ -13,6 +14,8 @@ export default function App() {
         <Features />
         <GalleryCarousel />
       </main>
+
+      <Footer />
     </div>
   );
 }
